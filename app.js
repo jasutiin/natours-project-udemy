f1172326d1dac65aadc2fbf322b71e4516711113
justine <justinemangaliman@gmail.com>
2024-10-01 23:21:10 -0600

    basic crud

---
+++ app.js
@@ -0,0 +1,104 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json()); // middleware, function that can modify req data
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+const getAllTours = (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', results: tours.length, data: { tours: tours } });
+};
+
+const getTour = (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+
+  const tour = tours.find((el) => el.id === id); // iterates through tours until el.id === req.params
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+const createTour = (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  ); // insted of writeFileSync because we are inside callback function that runs in event loop, cannot block event loop
+};
+
+const updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getAllTours)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on ${port}...`);
+});
