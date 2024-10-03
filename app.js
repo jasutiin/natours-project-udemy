3d0d94b1149d7114083b733741f8e384eba65ac9
Justine <justinemangaliman@gmail.com>
2024-10-02 20:43:41 -0600

    set up linter and such

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const app = express();
 
 // 1) MIDDLEWARE
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json()); // middleware, function that can modify req data
 app.use(express.static(`${__dirname}/public`));
 
